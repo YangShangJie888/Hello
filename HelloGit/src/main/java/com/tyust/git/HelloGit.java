8625df3f15ee90eeb138e580a9c9581c36a776a6
YangShangJie888 <914927545@qq.com>
2020-12-07 22:55:04 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/tyust/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.tyust.git;
+
+/**
+ * @author ysj
+ * @create 2020-12-07 {TIME}
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
